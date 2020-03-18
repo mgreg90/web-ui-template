ac1e45e61309a1ebf9af849afa638e60c5349ea3
Mike Gregory <mgregory8219@gmail.com>
2020-03-17 22:49:19 -0400

    Fixed build

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import leftPad from './leftpad';
 
-const serNos = [6934, 23111, 23114, 1001, 211161, 'waaa'];
+const serNos = [6934, 23111, 23114, 1001, 211161, 'oh hey what????', 'oh yeee?'];
 const partEl = document.getElementById('part-list');
 const strList = serNos.reduce(
   (acc, element) => acc += `<li>${leftPad(element, 8, '0')}</li>`, ''
